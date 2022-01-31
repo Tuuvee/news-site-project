2515f167f80368e144fb08c3a5118ef2133516c6
Tuuvee <tuukka.tahtinen@hotmail.fi>
2022-01-31 12:39:30 +0200

    Added react-router-dom

--- news-app/src/components/Content.js
+++ news-app/src/components/Content.js
@@ -1,4 +1,5 @@
 import ArticlePreview from './ArticlePreview';
+import {BrowserRouter as Router, Route, Routes,} from "react-router-dom";
 function Content() {
   return (
     <div className="content">
